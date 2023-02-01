c6937515b415606cab9044c0442f5d4a1dbf0a79
Neil <neilrowland56@yahoo.com>
2023-02-01 17:36:34 +0000

    Added first groq query to user page.js

--- app/(user)/page.js
+++ app/(user)/page.js
@@ -1,9 +1,25 @@
 import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+import { client } from "lib/sanity.client";
 
-export default function Home() {
+const query = groq`
+  *[_type=='post'] {
+    ...,
+    author->,
+    categories[]->
+  } | order(_createdAt desc)
+`;
+
+export default async function Home() {
   if (previewData()) {
-    return <div>Preview mode</div>;
+    return (
+      <div className="container mx-auto text-center text-7xl">Preview mode</div>
+    );
   }
+
+  const posts = await client.fetch(query);
+
+  console.log(posts);
   return (
     <section className="container mx-auto text-center">
       <h1>Not in Preview Mode</h1>
